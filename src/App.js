bc7b685a3d4cdfbdbe33a1e2d4cb120193a1a22d
주자훈 <jahoon@Jahoon-MacBookPro.local>
2025-05-11 23:55:40 +0900

    add:2025 alom to do list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-/**
- * 과제1-2: 이곳에 assignments 폴더 내 작성한 코드를 불러오는 코드를 작성해주세요.
- * 예시:
- * import Woojin from "./assignments/woojin";
- */
 import Example from "./assignments/example";
 
 function App() {
--- src/assignments/example.js
+++ src/assignments/example.js
@@ -1,13 +1,37 @@
 import { useState } from "react";
 
-function App() {
-  // 과제1-1: 7-1, 7-2강을 듣고 이곳에 투두리스트 컴포넌트를 작성해주세요.
-
+function Example() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      <h1>My To Dos</h1>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Enter text"
+        />
+        <button>Submit</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default Example;
